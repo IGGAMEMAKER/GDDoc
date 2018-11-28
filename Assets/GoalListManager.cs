272108abec15d1cf91d89e82c3df8e2e1a20565d
gaga <23i03g@mail.ru>
2018-11-28 21:03:02 +0400

    GoalListManager

---
+++ Assets/GoalListManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalListManager : MonoBehaviour {
+    bool isActive = false;
+
+    List<Goal> Goals;
+
+    public GameObject GoalListView;
+    GoalListView listView;
+
+	// Use this for initialization
+	void Start () {
+        Goals = new List<Goal>();
+        Goals.Add(new Goal("Strong Goal", "Descr"));
+
+        listView = GoalListView.GetComponent<GoalListView>();
+
+        Render();
+    }
+
+    public void AddGoal()
+    {
+
+    }
+
+    void Render()
+    {
+        listView.Initialize(Goals);
+    }
+}
--- Assets/GoalListView.cs
+++ Assets/GoalListView.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoalListView : MonoBehaviour {
-    List<Goal> goals;
+    List<Goal> Goals;
+    List<GameObject> Views;
+
+    public GameObject GoalPrefab;
 
 	// Use this for initialization
 	void Start () {
-		
+        Views = new List<GameObject>();
 	}
-	
+
+    public void Initialize(List<Goal> goals)
+    {
+        Goals = goals;
+
+        for (var i = 0; i < goals.Count; i++)
+        {
+            GameObject obj = Instantiate(GoalPrefab, transform, false);
+            obj.transform.localPosition = new Vector3(-i * 100, 0, 0);
+            Views.Add(obj);
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
