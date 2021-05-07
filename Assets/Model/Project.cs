8b557f710f88aaae93b9ab1f330123891667f285
gaga <23i03g@mail.ru>
2021-05-07 15:20:27 +0400

    Update Project.cs

--- Assets/Model/Project.cs
+++ Assets/Model/Project.cs
@@ -11,19 +11,15 @@ public class Vision
     public Idea Idea;
     public List<Player> Players;
 
-    // Success (Urge)
-    public List<Emotion> WhatFeelingsDoYouWantToCreate;
-    public List<string> WhatsFun;
-
     public string WhyThisWillWork; // cause people with traits X will get the ability and place to do Y
-
-    public string WhyPeopleCantIgnoreIt; // absurds, CTAs (clickbaits ------ calm stuff)
-    public string WhyPeopleWillBuyIt; // followed from the start, were engaged (played betas, gave feedback, spoke with dev)
-    public string WhyPeopleWillRecommend;
 }
 
 public class Idea
 {
+    // Success (Urge)
+    public List<Emotion> WhatFeelingsDoYouWantToCreate;
+    public List<string> WhatsFun;
+
     public string Goals;
     public string Challenge; // tough, but achievable
     public string Roles;
