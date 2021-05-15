fba2fa90b51df43162f8bee1dab49a401d4773b5
gaga <23i03g@mail.ru>
2021-05-16 02:07:52 +0400

    Update GDDoc.cs

--- Assets/GDDoc.cs
+++ Assets/GDDoc.cs
@@ -2,17 +2,20 @@
 using UnityEditor;
 using UnityEngine;
 
+// https://stackoverflow.com/questions/55407676/how-can-i-draw-a-list-and-all-its-items-in-inspector-editor-script
+
 public class GDDoc : EditorWindow
 {
     string test = "ghgh";
 
     public Dictionary<string, bool> Foldouts = new Dictionary<string, bool>();
+    public Vector2 scrollView = Vector2.zero;
 
     private void OnGUI()
     {
-        test = GetProp(test, "test");
+        //test = GetProp(test, "test");
 
-        EditorGUILayout.LabelField(test);
+        //EditorGUILayout.LabelField(test);
 
         var audience = new Audience
         {
@@ -49,7 +52,7 @@ public class GDDoc : EditorWindow
             Channels = new List<Channel>(),
         };
 
-        Project Project = new Project
+        Project project = new Project
         {
             WhatsFun = new List<string> { "Fun" },
             WhatFeelingsDoYouCreate = new List<Emotion> { new Emotion() },
@@ -62,49 +65,87 @@ public class GDDoc : EditorWindow
             Risks = new List<Risk>(),
         };
 
-        var myPropertyInfo = typeof(Project).GetFields();
+        //var myPropertyInfo = typeof(Project).GetFields();
 
-        //Debug.Log("Properties of Project are: " + myPropertyInfo.Length);
+        ////Debug.Log("Properties of Project are: " + myPropertyInfo.Length);
 
-        for (int i = 0; i < myPropertyInfo.Length; i++)
-        {
-            var info = myPropertyInfo[i];
+        //for (int i = 0; i < myPropertyInfo.Length; i++)
+        //{
+        //    var info = myPropertyInfo[i];
 
-            RenderFieldInfo(info, "");
-        }
+        //    RenderFieldInfo(info, "");
+        //}
+
+        scrollView = EditorGUILayout.BeginScrollView(scrollView);
 
         int depth = 0;
-        RenderParameter(Project, ref depth);
+        //RenderParameter(idea, ref depth);
+
+        //GUILayout.Space(25);
+        RenderParameter(project, ref depth);
+        //RenderParameter(typeof(Project), ref depth);
+
+
+        GUILayout.Space(25);
+        //RenderParameter(typeof(Project), ref depth);
+
+        EditorGUILayout.EndScrollView();
     }
 
     public void RenderParameter<T>(T parameter, ref int depth)
     {
         var myPropertyInfo = typeof(T).GetFields();
-        Debug.Log("parameter type " + parameter.GetType());
+
+        Debug.Log("CHECKING parameter type " + parameter);
 
         for (int i = 0; i < myPropertyInfo.Length; i++)
         {
             var info = myPropertyInfo[i];
 
-            //RenderFieldInfo(info, "");
-
-            //if (info.FieldType.IsEquivalentTo(typeof(string)))
-            //{
-            //    RenderFieldInfo(info, info.Name);
-            //}
-
-            //if (info.)
             depth++;
 
-            if (depth < 100)
-                RenderParameter(info.FieldType, ref depth);
+            var name = info.Name.ToString();
+            var fullName = depth + info.Name;
+
+            Debug.Log("Field: " + name + " " + fullName);
+
+            if (!Foldouts.ContainsKey(fullName))
+                Foldouts[fullName] = false;
+
+            EditorGUILayout.BeginFoldoutHeaderGroup(true, name);
+            //Foldouts[fullName] = EditorGUILayout.BeginFoldoutHeaderGroup(Foldouts[fullName], name);
+            //Foldouts[fullName] = EditorGUILayout.Foldout(Foldouts[fullName], name, true);
+
+            if (Foldouts[fullName] || true)
+            {
+                GUILayout.Label($"{info.FieldType} {fullName}");
+
+                RenderParameter(GetPropValue(parameter, info.Name), ref depth);
+                //RenderParameter(info.GetValue(info), ref depth);
+            }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
         }
+    }
+
+    public static object GetPropValue(object src, string propName)
+    {
+        // https://stackoverflow.com/questions/1196991/get-property-value-from-string-using-reflection
+        Debug.Log("Trying to get property " + propName + " from object " + src);
+
+        var value = src.GetType().GetField(propName).GetValue(src);
+
+        Debug.Log("Got " + value);
+
+        return value;
     }
 
     public void RenderFieldInfo(System.Reflection.FieldInfo info, string prefix)
     {
         var name = info.Name.ToString();
         var fullName = prefix + info.Name;
+
+        Debug.Log("Field: " + name + " " + fullName);
 
         if (!Foldouts.ContainsKey(fullName))
             Foldouts[fullName] = false;
@@ -114,16 +155,20 @@ public class GDDoc : EditorWindow
         if (Foldouts[fullName])
         {
             GUILayout.Label($"{info.FieldType} {fullName}");
-            //if (info.FieldType.IsEquivalentTo(typeof(string)))
-            //{
-            //    var val = "";
-            //    val = GetProp<string>(val, fullName);
-            //}
-            //else
-            //{
-            //    GUILayout.Label(name);
-            //}
         }
+    }
+
+    void RenderField()
+    {
+        //if (info.FieldType.IsEquivalentTo(typeof(string)))
+        //{
+        //    var val = "";
+        //    val = GetProp<string>(val, fullName);
+        //}
+        //else
+        //{
+        //    GUILayout.Label(name);
+        //}
     }
 
     //public string GetProp<T>(T str)
